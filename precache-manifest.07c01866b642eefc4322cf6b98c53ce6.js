021f34f70984ae6b816dad11d61c6595e6ade99d
Amanda Viceconte <amanda.viceconte@gmail.com>
2019-03-17 22:08:11 -0300

    Updates

---
+++ precache-manifest.07c01866b642eefc4322cf6b98c53ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd78f13f2eeb6b64bd90",
+    "url": "/resume/static/js/runtime~main.32a5f7b9.js"
+  },
+  {
+    "revision": "3efed68d19ea8eb0b310",
+    "url": "/resume/static/js/main.75d5ae11.chunk.js"
+  },
+  {
+    "revision": "b67db5639abad333c405",
+    "url": "/resume/static/js/2.c80588b1.chunk.js"
+  },
+  {
+    "revision": "3efed68d19ea8eb0b310",
+    "url": "/resume/static/css/main.ccb2e6ea.chunk.css"
+  },
+  {
+    "revision": "b67db5639abad333c405",
+    "url": "/resume/static/css/2.8f1b5d17.chunk.css"
+  },
+  {
+    "revision": "e0b1c028591c4530fda109b939a7a500",
+    "url": "/resume/index.html"
+  }
+];
